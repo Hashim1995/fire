7a81967a14007e95cf69d978222f0ddacee3ae0d
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-12-19 20:13:17 +0400

    lang done

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  ignoreDuringBuilds: true,
+};
 
 const withNextIntl = require("next-intl/plugin")(
   // This is the default (also the `src` folder is supported out of the box)
