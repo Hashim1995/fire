15471c71cf703b4e39f1fe3256c44cbddfb379a2
Hashim Hashimli <hashimli.hshm@gmail.com>
2023-12-28 18:21:05 +0400

    changes

--- app/[locale]/components/sections/ContactForm.js
+++ app/[locale]/components/sections/ContactForm.js
@@ -1,51 +1,210 @@
 'use client'
-
+import axios from 'axios';
+import { useTranslations } from 'next-intl';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { Input, FormFeedback, Button, Spinner } from 'reactstrap';
 
 const ContactForm = () => {
-    return <div className="col-xl-7 col-lg-6">
-        <div className="sec-title">
-            <span className="sub-title">Send us email</span>
-            <h2>Feel free to write</h2>
+    const [loading, setLoading] = useState(false);
+    const t = useTranslations()
+
+    const {
+        register,
+        reset,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        mode: 'onChange',
+        defaultValues: {
+            fullname: '',
+            email: '',
+            title: '',
+            phoneNumber: '',
+            description: '',
+        }
+    });
+
+    const onSubmit = async (data) => {
+        setLoading(true)
+        const payload = {
+            fullname: data?.fullname,
+            email: data?.email,
+            title: data?.title,
+            phoneNumber: data?.phoneNumber,
+            description: data?.description,
+        }
+        try {
+            const res = await axios.post('https://ivisaapp.azurewebsites.net/api/v1/appeal', payload);
+            console.log(res);
+            if (res?.data?.succeeded) {
+                reset();
+                toast.success(t("SuccessOperation"))
+
+            }
+        }
+        catch (err) {
+            toast.error(t("ErrorOperation"))
+        }
+        setLoading(false)
+
+    }
+
+    return (
+        <div className="col-xl-7 col-lg-6">
+            <div className="sec-title">
+                <span className="sub-title">{t("SendUsEmail")}</span>
+                <h2>{t("FeelFreeToWrite")}</h2>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="mb-3">
+                            <Controller
+                                control={control}
+                                rules={
+                                    { required: { value: true, message: `${t("Name")} ${t("IsRequired")}` } }
+                                }
+                                name="fullname"
+                                render={({ field: { onChange, value } }) => (
+                                    <Input
+                                        invalid={errors?.fullname ? true : false}
+                                        value={value}
+                                        onChange={onChange}
+                                        className="form-control"
+                                        type="text"
+                                        placeholder={t("EnterName")}
+                                    />
+                                )}
+                            />
+                            {errors.fullname && (
+                                <FormFeedback>{errors.fullname.message}</FormFeedback>
+                            )}
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="mb-3">
+                            <Controller
+                                control={control}
+                                rules={
+                                    {
+                                        required: { value: true, message: `${t("Email")} ${t("IsRequired")}` },
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: t("InvalidEmail")
+                                        }
+                                    }
+                                }
+                                name="email"
+                                render={({ field: { onChange, value } }) => (
+                                    <Input
+                                        invalid={errors?.email ? true : false}
+                                        value={value}
+                                        onChange={onChange}
+                                        className="form-control"
+                                        type="text"
+                                        placeholder={t("EnterEmail")}
+
+                                    />
+                                )}
+                            />
+                            {errors.email && (
+                                <FormFeedback>{errors.email.message}</FormFeedback>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="mb-3">
+                            <Controller
+                                control={control}
+                                rules={
+                                    { required: { value: true, message: `${t("Title")} ${t("IsRequired")}` } }
+                                }
+                                name="title"
+                                render={({ field: { onChange, value } }) => (
+                                    <Input
+                                        invalid={errors?.title ? true : false}
+                                        value={value}
+                                        onChange={onChange}
+                                        className="form-control"
+                                        type="text"
+                                        placeholder={t("EnterTitle")}
+                                    />
+                                )}
+                            />
+                            {errors.title && (
+                                <FormFeedback>{errors.title.message}</FormFeedback>
+                            )}
+
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="mb-3">
+                            <Controller
+                                control={control}
+                                name="phoneNumber"
+                                render={({ field: { onChange, value } }) => (
+                                    <Input
+                                        value={value}
+                                        onChange={onChange}
+                                        className="form-control"
+                                        type="text"
+                                        placeholder={t("EnterPhoneNumber")}
+                                    />
+                                )}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="mb-3">
+                    <Controller
+                        control={control}
+                        rules={
+                            { required: { value: true, message: `${t("Description")} ${t("IsRequired")}` } }
+                        }
+                        name="description"
+                        render={({ field: { onChange, value } }) => (
+                            <Input
+                                invalid={errors?.description ? true : false}
+                                value={value}
+                                onChange={onChange}
+                                className="form-control"
+                                rows="7"
+                                type="textarea"
+                                placeholder={t("EnterDescription")}
+
+                            />
+                        )}
+                    />
+                    {errors.description && (
+                        <FormFeedback>{errors.description.message}</FormFeedback>
+                    )}
+
+
+                </div>
+                <div className="mb-3">
+
+
+                    <Button disabled={loading} type="submit" className="theme-btn me-1 border-0 rounded-0 btn-style-one"><span className="btn-title text-white">{
+                        loading ?
+                            <Spinner
+                                style={{ width: "0.7rem", height: "0.7rem" }}
+                                type="grow"
+                                color="light"
+                            /> : t("SendMessage")}</span></Button>
+
+
+                    <button onClick={() => reset()} type="button" className="theme-btn btn-style-one bg-theme-color5">
+                        <span className="btn-title">{t("Reset")}</span>
+                    </button>
+                </div>
+            </form>
         </div>
-        <form id="contact_form" name="contact_form" className="" action="#" method="post">
-            <div className="row">
-                <div className="col-sm-6">
-                    <div className="mb-3">
-                        <input name="form_name" className="form-control" type="text" placeholder="Enter Name" />
-                    </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="mb-3">
-                        <input name="form_email" className="form-control required email" type="email" placeholder="Enter Email" />
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-sm-6">
-                    <div className="mb-3">
-                        <input name="form_subject" className="form-control required" type="text" placeholder="Enter Subject" />
-                    </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="mb-3">
-                        <input name="form_phone" className="form-control" type="text" placeholder="Enter Phone" />
-                    </div>
-                </div>
-            </div>
-            <div className="mb-3">
-                <textarea name="form_message" className="form-control required" rows="7" placeholder="Enter Message"></textarea>
-            </div>
-            <div className="mb-3">
-                <input name="form_botcheck" className="form-control" type="hidden" value="" />
-                <button type="submit" className="theme-btn btn-style-one mr-15" data-loading-text="Please wait...">
-                    <span className="btn-title">Send message</span>
-                </button>
-                <button type="reset" className="theme-btn btn-style-one bg-theme-color5">
-                    <span className="btn-title">Reset</span>
-                </button>
-            </div>
-        </form>
-    </div>
+    );
 };
 
 export default ContactForm;
